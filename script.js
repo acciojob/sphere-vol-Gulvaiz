282bc78c7a42613de1a39ea0b4dea9f6208547d1
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-01 16:08:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function volume_sphere() {
     //Write your code here
-  
+    let radius = document.getElementById("radius")
+	let volume = document.getElementById("volume")
+	let submit = document.getElementById("submit")
+
+let rad = radius.value
+	
+	submit.addEventListener("click", (event) => {
+           if(rad == -1 ){
+			   volume.value = "NaN"
+		   }
+			   else{
+		 
+		let vol = (4/3) * 3.14 * (rad^3)
+	         volumne.value = vol
+	})
+}
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
